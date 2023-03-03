ea95e2e18eb16175a209913c6c195411f195be01
Ezikielquin3 <111902653+Ezikielquin3@users.noreply.github.com>
2023-03-02 17:36:17 -0700

    Index.js update

--- Dump/index.js
+++ Dump/index.js
@@ -1,4 +1,106 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+  import { getdatabase } from "src="https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js"";
+  import { getauth } from "src="https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDy-YwzZAD-7kwfXJIDkYyl-pChGqOdr54",
+    authDomain: "the-950ec.firebaseapp.com",
+    databaseURL: "https://the-950ec-default-rtdb.firebaseio.com",
+    projectId: "the-950ec",
+    storageBucket: "the-950ec.appspot.com",
+    messagingSenderId: "921933112776",
+    appId: "1:921933112776:web:4a9093a12226845ecb0cfd",
+    measurementId: "G-XNLKNENNS7"
+  };
+
+</script>
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 
+  // Initialize variables 
+  const auth = firebase.auth()
+  const database = firebase.database()
+
+  //Set up our register function 
+  function register(){
+    // Get all our input fields 
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+
+    if(validate_email(email) == false || validate_password(password) == false) {
+     alert ('Email or Password is Invalid')
+      return
+    }
+
+    // Move on with Auth 
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      var user = auth.currentUser
+
+      // Add this user to Firebase Database 
+      var database_ref = database.ref()
+
+      //Create User Data 
+      var user_data = {
+        email : email,
+        last_login : Date.now()
+      }
+      
+      database_ref.child('users/' + user.uid).set(user_data)
+
+      alert('User Created !!')
+    })
+   
+   
+    .catch(function(error) {
+    var error_code = error.code 
+    var error_message = error.message 
+
+    alert(error_message)
+    })
+
+  }
+
+  // Validate input fields 
+
+  function validate_email(email){
+   expression = /^[^@]+@\w+(\.\w+)+\w$/
+   if(expression.test(email) == true){
+    // Email is good 
+    return true 
+   } else {
+    //Email is not good 
+    return false
+   }
+  }
+
+  function validate_password(password){
+    //Firebase only accepts length greater than 6 
+    if(password < 6){
+      return false
+    } else {
+      return true
+    }
+  }
+
+  function validate_field(field){
+    if(field == null){
+      return false
+    }
+    if (field.length <= 0){
+      return false
+    } else {
+      return true
+    }
+
+  }
 
 
