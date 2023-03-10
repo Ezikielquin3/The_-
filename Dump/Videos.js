306625d0ca84efa4a8af271bdceb36918c77153e
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-03-09 18:05:09 -0700

    videodatabase3

--- Dump/Videos.js
+++
@@ -1,7 +0,0 @@
-var data = [
-    {"id": "GsdSqBPV85Q", "title": "Introduction to Chest", "description": "", "category": "Chest"}
-]
-
-var root = document.getElementById('root');
-data.forEach(element => root.insertAdjacentHTML('beforebegin', `<tr><td>https://img.youtube.com/vi/${element.id}/hqdefault.jpg</td><td><div class="column"><h3>${title}</h3><p class="category">${category}</p><p>${description}</p></div></td></tr>`));
-//data.forEach(element => root.insertAdjacentHTML('beforebegin', `<tr><td>${element.id}</td><td>${title}</td></tr>`));
