a8222b8e387b81f3d0c4fb5300e2153a7953d5c6
Bu-Meh <meh5782@bears.unco.edu>
2023-03-09 19:00:33 -0700

    TRYING SOMETHING

--- Dump/auth.js
+++ Dump/auth.js
@@ -1,24 +1,5 @@
 
-import { initializeApp } from "../firebase/app";
-import { getdatabase } from "../irebase/database";
-import { getauth } from "../firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDy-YwzZAD-7kwfXJIDkYyl-pChGqOdr54",
-    authDomain: "the-950ec.firebaseapp.com",
-    databaseURL: "https://the-950ec-default-rtdb.firebaseio.com",
-    projectId: "the-950ec",
-    storageBucket: "the-950ec.appspot.com",
-    messagingSenderId: "921933112776",
-    appId: "1:921933112776:web:4a9093a12226845ecb0cfd",
-    measurementId: "G-XNLKNENNS7"
-  };
-  
-  firebase.intializeApp(firebaseConfig);
-  
-  // Initialize variables 
-  const auth = firebase.auth()
-  const database = firebase.database()
 
 function register(){
     // Get all our input fields 
