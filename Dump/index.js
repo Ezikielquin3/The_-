e717652d82cbf3e36276ace100c4f218b2460ca0
Ezikielquin3 <111902653+Ezikielquin3@users.noreply.github.com>
2023-03-09 17:44:36 -0700

    Config

--- Dump/index.js
+++ Dump/index.js
@@ -1,4 +1,5 @@
-var firebaseConfig = {
+import {intializeApp} from 'firebase/app'
+const firebaseApp = intializeApp({
   apiKey: "AIzaSyDy-YwzZAD-7kwfXJIDkYyl-pChGqOdr54",
   authDomain: "the-950ec.firebaseapp.com",
   databaseURL: "https://the-950ec-default-rtdb.firebaseio.com",
@@ -7,7 +8,7 @@ var firebaseConfig = {
   messagingSenderId: "921933112776",
   appId: "1:921933112776:web:4a9093a12226845ecb0cfd",
   measurementId: "G-XNLKNENNS7"
-};
+});
 
 firebase.intializeApp(firebaseConfig);
 
