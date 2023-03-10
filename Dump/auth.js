1747236033e5ed86d480e9a1607897f63d647904
Bu-Meh <meh5782@bears.unco.edu>
2023-03-09 19:15:46 -0700

    yyy

--- Dump/auth.js
+++ Dump/auth.js
@@ -61,20 +61,3 @@ function register(){
     }
   
   }
-
-const signupForm = document.querySelector('#signup');
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    //get user info
-    const email = signupForm['email'].value;
-    const password = signupForm['password'].value;
-    
-    // sign up the user 
-    AuthenticatorAssertionResponse.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-        const modal = document.querySelector('#modal-signup');
-        Map.Modal.getInstance(modal).close();
-        signupForm.reset();
-    })
-})
