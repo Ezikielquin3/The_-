acf7c00176e7d1cb3a87cc664e76532b669a402d
Bu-Meh <meh5782@bears.unco.edu>
2023-03-12 17:25:47 -0600

    something is working -- need help with firestore

--- Dump/auth.js
+++ Dump/auth.js
@@ -1,88 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
-import { getDatabase } from "../../node_modules/firebase/firebase-database.js";
-import { getAuth } from "../../node_modules/firebase/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+//Signup
+const signupForm = document.querySelector('#signup');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = initializeApp ({
-    apiKey: "AIzaSyDy-YwzZAD-7kwfXJIDkYyl-pChGqOdr54",
-    authDomain: "the-950ec.firebaseapp.com",
-    databaseURL: "https://the-950ec-default-rtdb.firebaseio.com",
-    projectId: "the-950ec",
-    storageBucket: "the-950ec.appspot.com",
-    messagingSenderId: "921933112776",
-    appId: "1:921933112776:web:4a9093a12226845ecb0cfd",
-    measurementId: "G-XNLKNENNS7"
-});
+  //get user info
 
-// Initialize Firebase
-firebase.intializeApp(firebaseConfig);
-// make auth and database references
-const auth = getAuth (firebaseApp);
-const database = getDatabase();
+  const email = signupForm['email'].value;
+  const password = signupForm['password'].value;
 
-// update database settings
-db.setting({ timestampsInSnapshots: true});
-function register(){
-    // Get all our input fields 
-    email = document.getElementById('email').value
-    password = document.getElementById('password').value
-  
-    if(validate_email(email) == false || validate_password(password) == false) {
-     alert ('Email or Password is Outta Line!!')
-      return
-    }
-  
-    // Move on with Auth 
-    auth.createUserWithEmailAndPassword(email, password)
-    .then(function() {
-  
-      var user = auth.currentUser
-  
-      // Add this user to Firebase Database 
-      var database_ref = database.ref()
-  
-      //Create User Data 
-      var user_data = {
-        email : email,
-        last_login : Date.now()
-      }
-      
-      database_ref.child('users/' + user.uid).set(user_data)
-  
-      alert('User Created !!')
-    })
-   
-   
-    .catch(function(error) {
-    var error_code = error.code 
-    var error_message = error.message 
-  
-    alert(error_message)
-    })
-  
-  }
+  //sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    const modal = document.querySelector('modal-signup');
+    Map.Modal.getInstance(modal).close();
+    signupForm.reset();
+  })
 
-  function validate_password(password){
-    //Firebase only accepts length greater than 6 
-    if(password < 6){
-      return false
-    } else {
-      return true
-    }
-  }
-  
-  function validate_field(field){
-    if(field == null){
-      return false
-    }
-    if (field.length <= 0){
-      return false
-    } else {
-      return true
-    }
-  
-  }
+})
