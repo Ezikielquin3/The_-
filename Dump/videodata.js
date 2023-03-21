06319113e1af0d776d62fe32bbfcb6b8c1158cfb
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-03-21 13:03:59 -0600

    updated database

--- Dump/videodata.js
+++ Dump/videodata.js
@@ -1,6 +1,12 @@
 var data = [
-    {"id": "GsdSqBPV85Q", "title": "Introduction to Chest", "description": "Hello everyone, I’m Blaine Acheson, and welcome back to Acheson Athletics! Today we are discussing the basics of chest exercises. The purpose of this video is to educate you on the specific muscles in the chest, some good practices when working chest, and a list of exercises and variations to get you started", "category": "Chest"},
-    {"id": "Nj1U75GbQDY", "title": "Bench Press", "description": "Hello everyone, I’m Blaine Acheson, and welcome back to Acheson Athletics! Today we are discussing the Bench Press exercise and its variations.", "category": "Chest"},
-    {"id": "pUm3DbSwwlU", "title": "Introduction to Arms", "description": "", "category": "Arms"},
-    {"id": "-JinLE0EIFU", "title": "Supinated Curls", "description": "", "category": "Arms"}
+    {"id": "GsdSqBPV85Q", "title": "Introduction to Chest",     "category": "Chest", "description": ""},
+    {"id": "Nj1U75GbQDY", "title": "Bench Press",               "category": "Chest", "description": ""},
+    {"id": "tZrX7ft9mDk", "title": "Chest Flys",                "category": "Chest", "description": ""},
+    {"id": "VcyPOHnl4aI", "title": "Push-Ups",                  "category": "Chest", "description": ""},
+    {"id": "pUm3DbSwwlU", "title": "Introduction to Arms",      "category": "Arms",  "description": ""},
+    {"id": "-JinLE0EIFU", "title": "Supinated Curls",           "category": "Arms",  "description": ""},
+    {"id": "p2xNyx9KJpA", "title": "Overhead Tricep Extensions","category": "Arms",  "description": ""},
+    {"id": "x-MtrAYtbDs", "title": "Introduction to Back",      "category": "Back",  "description": ""},
+    {"id": "ekfHyVJxBcQ", "title": "Good Mornings",             "category": "Back",  "description": ""},
+    {"id": "6QAl3mBa0qM", "title": "Dumbbell Rows",             "category": "Back",  "description": ""}
 ];
