0917f7ef9a49044685a168678c3f17652406a7ea
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-04-05 17:51:27 -0600

    article stuff interacting with home screen

---
+++ Dump/articledata.js
@@ -0,0 +1,6 @@
+var data2 = [
+    {"id": "1", "title": "Launch of the Website!",          "date": "4/5/23", "link": "", "splash": "", "content": ""},
+    {"id": "2", "title": "New Feature: Category Buttons",   "date": "4/5/23", "link": "", "splash": "", "content": ""},
+    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "", "content": ""},
+    {"id": "4", "title": "New Video: Crunches",             "date": "4/5/23", "link": "", "splash": "", "content": ""}
+]
