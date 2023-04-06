8d95172b441d09370026c7860a552751f51f0a3e
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-04-05 18:28:45 -0600

    uhh its not working

--- Dump/articledata.js
+++ Dump/articledata.js
@@ -1,6 +1,10 @@
 var data2 = [
-    {"id": "1", "title": "Launch of the Website!",          "date": "4/5/23", "link": "", "splash": "", "content": ""},
-    {"id": "2", "title": "New Feature: Category Buttons",   "date": "4/5/23", "link": "", "splash": "", "content": ""},
-    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "", "content": ""},
-    {"id": "4", "title": "New Video: Crunches",             "date": "4/5/23", "link": "", "splash": "", "content": ""}
-]
+    {"id": "1", "title": "Launch of the Website!",          "date": "4/5/23", "link": "", "splash": "Time to celebrate", 
+    "content": ""},
+    {"id": "2", "title": "New Feature: Category Buttons",   "date": "4/5/23", "link": "../views/Videos.html", "splash": "Don't worry, this didn't take longer to implement than it should have", 
+    "content": ""},
+    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "A great workout for you hamstrings", 
+    "content": ""},
+    {"id": "4", "title": "New Video: Crunches",             "date": "4/5/23", "link": "", "splash": "Refresh your memory on crunches with this video", 
+    "content": ""}
+];
