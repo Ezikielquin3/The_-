44570eabe68ad44730a336c25f9570e3c645f62d
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-04-05 19:32:51 -0600

    more interactable stuff in video page

--- Dump/articledata.js
+++ Dump/articledata.js
@@ -1,9 +1,9 @@
 
 //This data must be in order from oldest date to newest, otherwise it looks weird... so yeah//
 var data2 = [
-    {"id": "1", "title": "Launch of the Website!",          "date": "4/5/23", "link": "", "splash": "Time to celebrate, where's the champagne?", 
+    {"id": "1", "title": "Launch of the Website!",          "date": "1/26/23", "link": "", "splash": "Time to celebrate, where's the champagne?", 
     "content": ""},
-    {"id": "2", "title": "New Feature: Category Buttons",   "date": "4/5/23", "link": "../views/Videos.html", "splash": "Try them out on your next visit to the videos page", 
+    {"id": "2", "title": "New Feature: Category Buttons",   "date": "3/30/23", "link": "../views/Videos.html", "splash": "Try them out on your next visit to the videos page", 
     "content": ""},
     {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "A great workout for your hamstrings", 
     "content": ""},
