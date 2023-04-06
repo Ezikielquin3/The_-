d3fdc5bc0e0cbb5f8650f6503452e8c1a3485fb9
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-04-05 19:04:10 -0600

    ok it kinda works now

--- Dump/articledata.js
+++ Dump/articledata.js
@@ -1,9 +1,11 @@
+
+//This data must be in order from oldest date to newest, otherwise it looks weird... so yeah//
 var data2 = [
-    {"id": "1", "title": "Launch of the Website!",          "date": "4/5/23", "link": "", "splash": "Time to celebrate", 
+    {"id": "1", "title": "Launch of the Website!",          "date": "4/5/23", "link": "", "splash": "Time to celebrate, where's the champagne?", 
     "content": ""},
-    {"id": "2", "title": "New Feature: Category Buttons",   "date": "4/5/23", "link": "../views/Videos.html", "splash": "Don't worry, this didn't take longer to implement than it should have", 
+    {"id": "2", "title": "New Feature: Category Buttons",   "date": "4/5/23", "link": "../views/Videos.html", "splash": "Try them out on your next visit to the videos page", 
     "content": ""},
-    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "A great workout for you hamstrings", 
+    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "A great workout for your hamstrings", 
     "content": ""},
     {"id": "4", "title": "New Video: Crunches",             "date": "4/5/23", "link": "", "splash": "Refresh your memory on crunches with this video", 
     "content": ""}
