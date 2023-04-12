c45c9645cd0c5a9ad252a1289f52384b4e71b5c7
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-04-11 20:06:09 -0600

    article stuff is done

--- Dump/articledata.js
+++ Dump/articledata.js
@@ -1,12 +1,16 @@
 
 //This data must be in order from oldest date to newest, otherwise it looks weird... so yeah//
+//use this to make a new paragraph in content:  <br><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  //
 var data2 = [
-    {"id": "1", "title": "Launch of the Website!",          "date": "1/26/23", "link": "", "splash": "Time to celebrate, where's the champagne?", 
-    "content": ""},
-    {"id": "2", "title": "New Feature: Category Buttons",   "date": "3/30/23", "link": "../views/Videos.html", "splash": "Try them out on your next visit to the videos page", 
-    "content": ""},
-    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "A great workout for your hamstrings", 
-    "content": ""},
-    {"id": "4", "title": "New Video: Crunches",             "date": "4/5/23", "link": "", "splash": "Refresh your memory on crunches with this video", 
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+    {"id": "1", "title": "Launch of the Website!",          "date": "1/26/23", "link": "", "splash": "Time to celebrate, where's the champagne?", "author": "The_: Team",
+    "content": "It's finally here after long last! This project has definitely had its ups and downs, but I couldn't be prouder of our team here at The_:. Although we have had to drastically reduce our scale, especially in the ''user account'' oriented features, I think we ended up pulling through with a pretty feature-packed website. Bu and Zeke worked tremendously hard to get the user database in a functional state the entire development period, and that alone I think requires commendation. Kyle was a great team member, always asking the important questions, and building a robust badge system, even if it isn't fully implemented into the database as of writing. Blaine, our project sponsor, also put a tremendous amount of leg-work in, writing 90% of all scripts used in our videos and being the star of the show. Overall, I couldn't ask for a better team. -Thomas Avinger, Web Developer "},
+
+    {"id": "2", "title": "New Feature: Category Buttons",   "date": "3/30/23", "link": "", "splash": "Try them out on your next visit to the videos page", "author": "Thomas Avinger, Web Developer",
+    "content": "Hello! Its me again here to rant about how this took longer than it should have. Regardless, I'm glad that the end result turned out to be a perfect copy of my vision. Shoutout to Zeke for reccomending this feature to be implemented. The page definitely needed a function like this, especially as our video catalog grows in size over our website's lifespan. Okay, lets talk about how to use them! They're pretty simple. Each button correlates with a muscle category. Click the button that correlates with the category you want to see. The page will reload, only showing those videos. You can also click the ''Video Catalog'' button at the top to reset the page to show all videos again. Ok, that's all for now. I'll let you know when I add more buttons to the site."},
+
+    {"id": "3", "title": "New Video: Romanian Deadlifts",   "date": "4/5/23", "link": "", "splash": "A great workout for your hamstrings", "author": "Thomas Avinger, Video Producer",
+    "content": "A new video for the Legs category is out now! Watch the Romanian Deadlifts video today!"},
+
+    {"id": "4", "title": "New Video: Crunches",             "date": "4/5/23", "link": "", "splash": "Refresh your memory on crunches with this video", "author": "Thomas Avinger, Video Producer",
+    "content": "A new video for the Core category is out now! Watch the Crunches video today!"},
 ];
