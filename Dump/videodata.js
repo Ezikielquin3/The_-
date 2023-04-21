3c671490640b9b05d1e73c302cc1479c0d49fb9d
ThomasSandwich <122579609+ThomasSandwich@users.noreply.github.com>
2023-04-21 14:34:47 -0600

    started work on new video section on home page

--- Dump/videodata.js
+++ Dump/videodata.js
@@ -1,15 +1,16 @@
 //Data tuples are organized by category, and then date published, except the intro videos which come first in a category. In the future, I'll add some sorting into the JS to fix this specificity//
+//Order is the order in which the videos came out, with the lowest number being the first released and vice versa//
 var data = [
-    {"id": "GsdSqBPV85Q", "title": "Introduction to Chest",     "category": "Chest", "description": ""},
-    {"id": "Nj1U75GbQDY", "title": "Bench Press",               "category": "Chest", "description": ""},
-    {"id": "tZrX7ft9mDk", "title": "Chest Flys",                "category": "Chest", "description": ""},
-    {"id": "VcyPOHnl4aI", "title": "Push-Ups",                  "category": "Chest", "description": ""},
-    {"id": "pUm3DbSwwlU", "title": "Introduction to Arms",      "category": "Arms",  "description": ""},
-    {"id": "-JinLE0EIFU", "title": "Supinated Curls",           "category": "Arms",  "description": ""},
-    {"id": "p2xNyx9KJpA", "title": "Overhead Tricep Extensions","category": "Arms",  "description": ""},
-    {"id": "x-MtrAYtbDs", "title": "Introduction to Back",      "category": "Back",  "description": ""},
-    {"id": "ekfHyVJxBcQ", "title": "Good Mornings",             "category": "Back",  "description": ""},
-    {"id": "6QAl3mBa0qM", "title": "Dumbbell Rows",             "category": "Back",  "description": ""},
-    {"id": "-WPaMfs878A", "title": "Romanian Deadlifts",        "category": "Legs",  "description": ""},
-    {"id": "1dj_Ojm52zg", "title": "Crunches",                  "category": "Core",  "description": ""}
+    {"id": "GsdSqBPV85Q", "title": "Introduction to Chest",     "category": "Chest", "order": "1",  "description": ""},
+    {"id": "Nj1U75GbQDY", "title": "Bench Press",               "category": "Chest", "order": "4",  "description": ""},
+    {"id": "tZrX7ft9mDk", "title": "Chest Flys",                "category": "Chest", "order": "7",  "description": ""},
+    {"id": "VcyPOHnl4aI", "title": "Push-Ups",                  "category": "Chest", "order": "10", "description": ""},
+    {"id": "pUm3DbSwwlU", "title": "Introduction to Arms",      "category": "Arms",  "order": "2",  "description": ""},
+    {"id": "-JinLE0EIFU", "title": "Supinated Curls",           "category": "Arms",  "order": "5",  "description": ""},
+    {"id": "p2xNyx9KJpA", "title": "Overhead Tricep Extensions","category": "Arms",  "order": "8",  "description": ""},
+    {"id": "x-MtrAYtbDs", "title": "Introduction to Back",      "category": "Back",  "order": "3",  "description": ""},
+    {"id": "ekfHyVJxBcQ", "title": "Good Mornings",             "category": "Back",  "order": "6",  "description": ""},
+    {"id": "6QAl3mBa0qM", "title": "Dumbbell Rows",             "category": "Back",  "order": "9",  "description": ""},
+    {"id": "-WPaMfs878A", "title": "Romanian Deadlifts",        "category": "Legs",  "order": "11", "description": ""},
+    {"id": "1dj_Ojm52zg", "title": "Crunches",                  "category": "Core",  "order": "12", "description": ""}
 ];
